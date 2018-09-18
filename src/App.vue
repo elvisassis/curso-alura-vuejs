5134b4999bacc9008c3171566d6af044c4de79e4
Elvis Assis <kesleiassis@hotmail.com>
2018-09-17 21:58:34 -0300

    arquivos projeto

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+	<h1>{{titulo}}</h1>
+	<ul>
+		<li v-for="(foto, index) in fotos" :key="index">
+			<img :src="foto.url" :alt="foto.titulo">
+		</li>
+	</ul>
+</div> 
+</template>
+
+<script>
+export default {
+	data(){
+
+		return {
+			titulo: 'Alurapick',
+			fotos: [
+				{
+					url: "https://t1.ea.ltmcdn.com/pt/images/5/3/1/o_que_fazer_se_o_meu_cachorro_esta_estressado_20135_600.jpg",
+					titulo: "cachorro"
+				},
+				{
+					url: "https://t1.ea.ltmcdn.com/pt/images/5/3/1/o_que_fazer_se_o_meu_cachorro_esta_estressado_20135_600.jpg",
+					titulo: "cachorrão"
+				}]
+		}
+	}
+}  
+</script>
+
+<style>
+</style>
